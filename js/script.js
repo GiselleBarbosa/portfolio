e7960232cc91b435829a9b35ee09ea2006c3c2da
GiselleBarbosa <g88barbosa@gmail.comm>
2023-06-15 01:16:27 -0300

    chore: modified style and update some info

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
 function openNav() {
+  var x = document.getElementById("navigation");
 
-    var x = document.getElementById("navigation");
-
-    if (x.className === "navigation") {
-        x.className += " menujs";
-        document.getElementById("threeline").innerHTML = "&Cross;";
-        
-    } else {
-        x.className = "navigation";
-        document.getElementById("threeline").innerHTML = "&#9776;";
-    }   
-  
+  if (x.className === "navigation") {
+    x.className += " menujs";
+    document.getElementById("threeline").innerHTML = "&Cross;";
+  } else {
+    x.className = "navigation";
+    document.getElementById("threeline").innerHTML = "&#9776;";
+  }
 }
 
 var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
